3c542421b2e657b6960d204f9a7372430cf16834
5W4RUP <saren.swarup100@gmail.com>
2024-04-16 00:03:21 +0530

    master part

--- reduxTK/src/components/AddTodo.jsx
+++ reduxTK/src/components/AddTodo.jsx
@@ -21,7 +21,7 @@ function AddTodo() {
                 placeholder="Enter a Todo..."
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
-            />
+            />{{ input }}
             <button
                 type="submit"
                 className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
