95fcd66c6f9411aeb6bd46a01f77468771394a84
5W4RUP <saren.swarup100@gmail.com>
2024-05-20 21:46:01 +0530

    music

--- next/musicofsilence/src/app/page.tsx
+++ next/musicofsilence/src/app/page.tsx
@@ -1,10 +1,14 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-3xl text-center dark:text-white">Chai aur code</h1>
+      <h1 className="text-3xl text-center dark:text-white">Header</h1>
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 }
